fa73c126239b01adc82a1628e27f1ddd731f7c7a
owisg <owisgamal.2020@gmail.com>
2023-10-15 12:01:32 -0700

    update 7A

--- main.h
+++ main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <limits.h>
+#include <stdlib.h>
 
 int _putchar(char character);
 int print_char(va_list args, int printed);
