abb08082782773880c5216b1f370521c99d82a19
shimaahriaz <shimaahriaz@gmail.com>
2023-10-15 08:16:25 -0700

    S

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _putchar(char character);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_int(va_list args, int printed);
+int my_choice(const char *format, va_list args, int printed);
+printf_binary(va_arg(args, unsigned int), printed);
+int _printf(const char *format, ...);
+int print_hexa(unsigned int num, int printed, int uppercase);
+int print_oct(unsigned int num, int printed);
+int print_unsigned(unsigned int num, int printed);
+
+#endif
+
